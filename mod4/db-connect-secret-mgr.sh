af008a621855d48c3e3459073b761832ccf07004
shotagtag <136060153+shotagtag@users.noreply.github.com>
2025-11-27 21:00:45 +0900

    Add script to connect to MySQL using Secrets Manager
    This script retrieves database connection information from AWS Secrets Manager and connects to a MySQL database using the extracted credentials.

---
+++ mod4/db-connect-secret-mgr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Secrets Manager から接続情報を取得（JSON形式）
+SECRET=$(aws secretsmanager get-secret-value --secret-id "mydatabase/credentials" --query "SecretString" --output text)
+
+
+# JSONから各値を抽出
+DB_HOST=$(echo $SECRET | jq -r '.host')
+DB_USER=$(echo $SECRET | jq -r '.username')
+DB_PASS=$(echo $SECRET | jq -r '.password')
+DB_NAME=$(echo $SECRET | jq -r '.database')
+
+# MySQLデータベースに接続
+mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME
